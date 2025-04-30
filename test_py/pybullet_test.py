c9cc83ec20f479dc8f127d64d605da47e52701c0
Zote <bgwen586@gmail.com>
2025-04-30 22:37:46 +0800

    修改模型

--- test_py/pybullet_test.py
+++ test_py/pybullet_test.py
@@ -2,11 +2,18 @@ import pybullet as p
 import pybullet_data
 import time
 
+# 连接到 PyBullet GUI
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
-robot_id = p.loadURDF("tita/urdf/robot.urdf")
+
+# 加载地板和设置重力
+p.loadURDF("plane.urdf")
+p.setGravity(0, 0, -9.81)
+
+# 加载机器人模型
+robot_id = p.loadURDF("tita/urdf/robot.urdf", basePosition=[0, 0, 1])
 
 # 保持程序运行
 while True:
     p.stepSimulation()
-    time.sleep(1.0 / 240.0)  # 设置模拟步长
+    time.sleep(1.0 / 240.0)
